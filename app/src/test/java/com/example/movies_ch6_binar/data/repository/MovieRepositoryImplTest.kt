2caabbb21f8b54b60a2c98131060d6f19ce07168
Daud A rya Rafa <20082010137@student.upnjatim.ac.id>
2022-11-06 18:50:57 +0700

    Commiting Unit Test and Firebase Features

---
+++ app/src/test/java/com/example/movies_ch6_binar/data/repository/MovieRepositoryImplTest.kt
@@ -0,0 +1,50 @@
+package com.example.movies_ch6_binar.data.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.movies_ch6_binar.Models.usecase.movielist.GetMovieListUseCase
+import com.example.movies_ch6_binar.data.remote.dto.DetailDto
+import com.example.movies_ch6_binar.data.remote.dto.MovieDto
+import com.example.movies_ch6_binar.data.remote.dto.ResultDto
+import com.example.movies_ch6_binar.data.remote.dto.TheMovieDbApi
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.stubbing.OngoingStubbing
+import retrofit2.Response
+
+
+class MovieRepositoryImplTest{
+    private lateinit var api: TheMovieDbApi
+    private lateinit var repository: MovieRepositoryImpl
+
+
+    @Before
+    fun setUp() {
+        api = mock()
+        repository = MovieRepositoryImpl(api)
+    }
+
+    @Test
+    fun getMovieById() = runBlocking {
+        val correct = mockk<DetailDto>()
+        Mockito.`when`(api.getMovieById(any(), any())).thenReturn(correct)
+        val response = repository.getMovieById(32)
+        assertEquals(correct, response)
+    }
+
+
+
+}
+
+
+
+
+
