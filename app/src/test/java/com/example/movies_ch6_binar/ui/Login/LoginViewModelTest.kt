6694e3cd8925ac46f75aa0461c4722613938910b
Daud A rya Rafa <20082010137@student.upnjatim.ac.id>
2022-11-06 23:55:25 +0700

    Commiting Unit Test and Firebase Features

--- app/src/test/java/com/example/movies_ch6_binar/ui/Login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/movies_ch6_binar/ui/Login/LoginViewModelTest.kt
@@ -1,30 +1,16 @@
 package com.example.movies_ch6_binar.ui.Login
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.movies_ch6_binar.Models.repository.AuthRepository
-import com.example.movies_ch6_binar.data.local.entity.UserEntity
-import com.example.movies_ch6_binar.data.local.room.dao.UserDao
-import com.example.movies_ch6_binar.data.repository.AuthRepositoryImpl
-import io.mockk.MockKAnnotations
-import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
-import junit.framework.Assert
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
-import org.junit.Before
-import org.junit.Rule
 import org.junit.jupiter.api.Assertions.*
 
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
 
-class LoginViewModelTest {
+internal class LoginViewModelTest {
 
+    @Test
+    fun login() {
     }
 
-
-
-
-
+    @Test
+    fun setUser() {
+    }
+}
